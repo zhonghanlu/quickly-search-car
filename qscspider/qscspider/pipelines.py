bf93a8727d2dc0cff0d3068c71b834cc603d9833
zhonghanlu <1420865757@qq.com>
2023-10-13 11:07:50 +0800

    car_home_brands.csv init

--- qscspider/qscspider/pipelines.py
+++ qscspider/qscspider/pipelines.py
@@ -2,10 +2,8 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-import json
-
+import csv
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
 
 
 class QscspiderPipeline:
@@ -13,26 +11,38 @@ class QscspiderPipeline:
         return item
 
 
-
 class BrandPipeline(object):
 
     def __init__(self):
-        self.file = open('car_brand.json', 'w', encoding='utf-8')
+        # csv文件的位置,无需事先创建
+        store_file = 'car_home_brands.csv'
+        # 打开(创建)文件
+        self.file = open(store_file, 'a+', encoding="utf-8", newline='')
+        # csv写法
+        self.writer = csv.writer(self.file, dialect="excel")
 
     def process_item(self, item, spider):
         # 将item对象强制转为字典，该操作只能在scrapy中使用
         item = dict(item)
-        # 爬虫文件中提取数据的方法每yield一次，就会运行一次
-        # 该方法为固定名称函数
-        # 默认使用完管道，需要将数据返回给引擎
-        # 1.将字典数据序列化
-        '''ensure_ascii=False 将unicode类型转化为str类型，默认为True'''
-        json_data = json.dumps(item, ensure_ascii=False, indent=2) + ',\n'
-
-        # 2.将数据写入文件
-        self.file.write(json_data)
-
+        # 判断字段值不为空再写入文件
+        if item['start']:
+            self.writer.writerow([item['start'], item['brand'], item['categorize'], item['name']])
         return item
+
+    # def process_item(self, item, spider):
+    #     # 将item对象强制转为字典，该操作只能在scrapy中使用
+    #     item = dict(item)
+    #     # 爬虫文件中提取数据的方法每yield一次，就会运行一次
+    #     # 该方法为固定名称函数
+    #     # 默认使用完管道，需要将数据返回给引擎
+    #     # 1.将字典数据序列化
+    #     '''ensure_ascii=False 将unicode类型转化为str类型，默认为True'''
+    #     json_data = json.dumps(item, ensure_ascii=False, indent=2) + ',\n'
+    #
+    #     # 2.将数据写入文件
+    #     self.file.write(json_data)
+    #
+    #     return item
 
     def __del__(self):
         self.file.close()
